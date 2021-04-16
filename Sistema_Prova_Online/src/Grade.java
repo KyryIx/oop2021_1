3c105515d54095dabb1255a653a09beeb9401cfb
KyryIx <kyry.ix@gmail.com>
2021-04-15 21:26:49 -0300

    Inclusao das classes Curso, Grade, Nota, Questao e Turma

---
+++ Sistema_Prova_Online/src/Grade.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Grade {
+	private ArrayList<ArrayList<Disciplina>> disciplinas;
+
+	public ArrayList<ArrayList<Disciplina>> getDisciplinas() {
+		return this.disciplinas;
+	}
+
+	public void setDisciplinas(ArrayList<ArrayList<Disciplina>> disciplinas) {
+		this.disciplinas = disciplinas ;
+	}
+}
---
+++ Sistema_Prova_Online/src/Turma.java
@@ -0,0 +1,20 @@
+public class Turma {
+	private int codigo;
+	private Curso curso;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public Curso getCurso() {
+		return curso;
+	}
+	
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+}
